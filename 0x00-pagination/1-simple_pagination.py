54a24ec5b48c205716728267dee1ad8d11cab140
erdey <erdeysyoum@gmail.com>
2024-07-20 07:23:03 +0000

    apply an assert

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,10 +3,11 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """ pagination function """
-        start = (page - 1) * page_size
-        end = start + page_size
-        return (start, end)
+    """ pagination function """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -27,13 +28,11 @@ class Server:
 
         return self.__dataset
 
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert type(page) == int and type(page_size) == int
-            assert page > 0 and page_size > 0
-            start, end = index_range(page, page_size)
-            data = self.dataset()
-            
-            if start > len(data):
-                return []
-            return data[start:end]
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
