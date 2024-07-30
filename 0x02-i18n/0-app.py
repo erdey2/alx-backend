19d1d77e5bdcc5f46c1e0182aa70954a66015732
erdey <erdeysyoum@gmail.com>
2024-07-30 11:39:39 +0000

    initial Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Initial flask app """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+
+def welcome():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
