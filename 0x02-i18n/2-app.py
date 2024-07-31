f3403ecffe7fc3b9ab16e08fc128345ff9d7ede1
erdey <erdeysyoum@gmail.com>
2024-07-31 10:41:41 +0000

    select best language

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Basic Babel setup  """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """configuration Class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+@babel.localselector
+def get_local():
+    return request.accept_languages.best_match(app.config['LNAGUAGES'])
+
+
+@app.route("/")
+def welcome():
+    """ Route path """
+    return render_template("2-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
