b29175768fcf6b8cdb81bf79a33c6fb83772f4da
erdey <erdeysyoum@gmail.com>
2024-08-02 12:49:25 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,11 +22,9 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """
-    Gets locale from request object
-    """
-    locale = request.args.get('locale', '').strip()
-    if locale and locale in Config.LANGUAGES:
+    """ Gets locale from request object """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
